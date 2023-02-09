c03111b7cade4ffb5489779741d760d85d7fb998
rachellewilson <rachellewilson0507@gmail.com>
2023-02-08 22:41:50 -0800

    added layers of gameplay

--- index.js
+++ index.js
@@ -1,1 +1,108 @@
 //Your Code here
+
+let firstAnswer = window.prompt("Do you head left or right?")
+console.log(firstAnswer)
+
+console.log(firstAnswer === "left") 
+console.log(firstAnswer === "right")
+
+
+//true false conditional statement
+if (firstAnswer === "left") {{
+    let secondAnswer = window.prompt("There is a crocodile ahead. Do you head left or right?")
+    console.log(secondAnswer) 
+
+console.log(secondAnswer === "left")
+console.log(secondAnswer === "right")
+
+        if (secondAnswer === "left")
+        window.alert("Repeating bad decisions is the definition of insanity. You got eaten by the crocodile.")
+        
+        else if (secondAnswer === "right") {
+        let thirdAnswerLeftRight = window.prompt("You found an underground cavern. Upon entering, do you head left or right?")
+        console.log(thirdAnswerLeftRight)}
+
+console.log(thirdAnswerLeftRight === "left")
+console.log(thirdAnswerLeftRight === "right")
+
+            if (thirdAnswerLeftRight === "left"){
+            let fourthAnswer = window.prompt("You stumble upon a cave-dwelling bomtukyl. Do you run or fight?")
+            console.log(fourthAnswer)}
+
+console.log(fourthAnswer === "run")
+console.log(fourthAnswer === "fight")
+
+                if (fourthAnswer === "run"){}
+                let fifthAnswerRun = window.alert("Your wisdom rewards you. You run into a large, open cavern and find a dragon egg! When it hatches your adventures will continue...")
+                console.log(fifthAnswerRun)}
+                if (fourthAnswer === "fight"){
+                let fifthAnswerFight = window.alert ("You were doomed to failure before you began. At least your death was swift.")
+                console.log(fifthAnswerFight)}
+                else{ window.alert("Your lack of appropriate decision led to your demise. It was a tragic loss.")}}
+            if (thirdAnswerLeftRight === "right"){
+            window.alert("You stepped on a fine wire which activated a trap door. You have fallen into the abyss. You will be missed.")
+            }
+
+else if (firstAnswer === "right") {
+let secondAnswerRight = window.prompt("You boarded a pirate air-ship. Do you head for Mars or Jupiter?")
+console.log(secondAnswerRight)}
+
+console.log(secondAnswerRight === "Mars")
+console.log(secondAnswerRight === "Jupiter")
+
+    if (secondAnswerRight === "Mars"){
+    let MarsAnswer = window.prompt("You land on the planet and find a cave. Upon entering, do you head left or right?")
+    console.log(MarsAnswer)}
+
+console.log(MarsAnswer === "left")
+console.log(MarsAnswer === "right")
+
+        if (MarsAnswer === "right"){
+        let MarsAnswerRight = window.alert("You stepped on a fine wire which activated a trap door. You have fallen into the abyss. You will be missed.")
+        console.log(MarsAnswerRight)}
+        else if (MarsAnswer === "left"){
+        let MarsAnswerLeft = window.prompt("You stumble upon a cave-dwelling bomtukyl. Do you run or fight?")
+        console.log(MarsAnswerLeft)}
+
+console.log(MarsAnswerLeft === "run")
+console.log(MarsAnswerLeft === "fight")
+
+            if (MarsAnswerLeft === "run"){
+            let runAnswer = window.alert("Your wisdom rewards you. You run into a large, open cavern and find a dragon egg! When it hatches your adventures will continue...")
+            console.log(runAnswer)}
+            else if (MarsAnswerLeft === "fight"){
+            let fightAnswer = window.alert ("You were doomed to failure before you began. At least your death was swift.")
+            console.log(fightAnswer)}
+            else{
+            window.alert("Your lack of appropriate decision led to your demise. It was a tragic loss.")}
+        if (MarsAnswer === "right"){
+        let trapAnswer = window.alert("You stepped on a fine wire which activated a trap door. You have fallen into the abyss. You will be missed.")
+        console.log(trapAnswer)} 
+    else if (secondAnswerRight === "Jupiter"){
+    let JupiterAnswer = window.alert("You burned up on planetary entry.")
+    console.log(JupiterAnswer)
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
